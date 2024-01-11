39d55b43263c1e9e139e948b25fb5424a290512a
rootbmb <rootbmb@gmail.com>
2024-01-11 15:24:31 +0300

    fix1

--- exercises/04_data_structures/task_4_3.py
+++ exercises/04_data_structures/task_4_3.py
@@ -22,3 +22,4 @@
 """
 
 config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+print(config.split()[-1].split(','))
--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -22,9 +22,14 @@ ospf_route. Значения из строки ospf_route надо получи�
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
 template = """
-Prefix                {}
-AD/Metric             {}
-Next-Hop              {}
-Last update           {}
-Outbound Interface    {}
+Prefix                {:16}
+AD/Metric             {:16}
+Next-Hop              {:16}
+Last update           {:16}
+Outbound Interface    {:16}
 """
+ospf_route = ospf_route.split()
+ospf_route.remove('via')
+
+
+print(template.format(ospf_route[0], ospf_route[1].strip('[]'), ospf_route[2].rstrip(','), ospf_route[3].rstrip(','), ospf_route[4].rstrip(',')))
