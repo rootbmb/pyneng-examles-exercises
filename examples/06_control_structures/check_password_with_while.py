050977484a09711035203cd0d335bdd76dc2d250
rootbmb <rootbmb@gmail.com>
2024-01-13 23:18:33 +0300

    form

--- examples/06_control_structures/check_password_with_while.py
+++ examples/06_control_structures/check_password_with_while.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
-username = input('Введите имя пользователя: ')
-password = input('Введите пароль: ')
+username = input("Введите имя пользователя: ")
+password = input("Введите пароль: ")
 
 password_correct = False
 
 while not password_correct:
     if len(password) < 8:
-        print('Пароль слишком короткий\n')
-        password = input('Введите пароль еще раз: ')
+        print("Пароль слишком короткий\n")
+        password = input("Введите пароль еще раз: ")
     elif username in password:
-        print('Пароль содержит имя пользователя\n')
-        password = input('Введите пароль еще раз: ')
+        print("Пароль содержит имя пользователя\n")
+        password = input("Введите пароль еще раз: ")
     else:
-        print('Пароль для пользователя {} установлен'.format(username))
+        print("Пароль для пользователя {} установлен".format(username))
         password_correct = True
 
-'''
+"""
 Example:
 $ python check_password_with_while.py
 Введите имя пользователя: nata
@@ -28,4 +28,4 @@ $ python check_password_with_while.py
 
 Введите пароль еще раз: 123345345345
 Пароль для пользователя nata установлен
-'''
+"""
